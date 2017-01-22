3e7b82d7bc3c1480f138a57e87c0c2653cc731ae
Mikhail <uroganoff@gmail.com>
2017-01-22 09:57:09 +0000

    2nd commit

--- js.js
+++ js.js
@@ -3,27 +3,24 @@ window.onload =function ()
 
 var pos = 0;
 var box = document.getElementById('box');
-var box1 = document.getElementById('box1');
-var box2 = document.getElementById('box2');
-var box3 = document.getElementById('box3');
-var speed= Math.random()*50;
-var t = setInterval(move, speed);
+
+var speed= Math.random()*1;
+var t = setInterval(move, 0.5);
+
+var size = window.innerWidth || document.body.clientWidth;
+
 
 
 function move(){
-    if (pos >= 150){
+    if (pos >= size - 300 ){
         
         clearInterval(t);
-      
-      
     }
     else
     {
         pos +=1;
         box.style.left = pos+'px';
-                box1.style.left = pos+'px';
-                        box2.style.left = pos+'px';
-                                box3.style.left = pos+'px';
+          
     }
 }
   
