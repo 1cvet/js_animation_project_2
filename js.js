ecdaa4a257c95cf657ff6b65865dc0df3959075b
Mikhail <uroganoff@gmail.com>
2017-01-29 21:11:38 +0000

    new vers

--- js.js
+++ js.js
@@ -14,9 +14,10 @@ var speed= Math.random()*10;
 var speed2= Math.random()*10;
 var t = setInterval(move, speed);
 var t2 = setInterval(move2, speed2);
-var size = window.innerWidth || document.body.clientWidth;
+var size = document.getElementById("container").clientWidth - document.getElementById("img").clientWidth;
 
 
+/*
 var h3 = document.getElementById("h3"),
 h2byName = document.getElementsByTagName("h3"),
 myBody = document.getElementsByTagName("body")[0];
@@ -26,32 +27,35 @@ h32byName = document.getElementsByTagName("h3"),
 my2Body = document.getElementsByTagName("body")[1];
 
 h2byName[0].innerHTML =  "Speed : " + speed; 
-h32byName[1].innerHTML =  "Speed : " + speed2; 
+h32byName[1].innerHTML =  "Speed : " + speed2;  */
 
 function whoWin(){
 if (speed < speed2)
 {
        srtBtn.disabled=false;
-    alert("1st runner won!"); 
-}
-else if (speed == speed2)
-{
+    sweetAlert("1st runner won!"); 
+
    
-  
-     alert("Draw!"); 
+}
+else if (speed > speed2)
+{
+        srtBtn.disabled=false;
+sweetAlert("2nd runner won!");
     
 }
-else
+else 
 {
-       srtBtn.disabled=false;
-alert("2nd runner won!"); 
+    sweetAlert("Draw!"); 
+
 }
 }
 
 function move(){
-    if (pos >= size-300 ){
+    
+    if (pos >= size){
         
         clearInterval(t);
+         whoWin();
     }
     else
     {
@@ -62,7 +66,7 @@ function move(){
 }
 
 function move2(){
-    if (pos2 >= size-300 ){
+    if (pos2 >= size){
         
         clearInterval(t2);
         whoWin();
@@ -74,6 +78,6 @@ function move2(){
     }
 }
     
-};
+}
 
 
